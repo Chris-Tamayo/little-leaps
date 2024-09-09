7260a4c4364ba67768a1de0bda0a15f2ffc2f5b8
Christopher Tamayo <chriswtamayo@gmail.com>
2024-09-09 15:32:09 -0700

    Simplify annotations with Lombok

--- server/src/main/java/com/example/server/model/Task.java
+++ server/src/main/java/com/example/server/model/Task.java
@@ -2,6 +2,7 @@ package com.example.server.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
@@ -12,17 +13,9 @@ import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortK
 @Data
 @DynamoDbBean
 public class Task {
+    @Getter(onMethod_=@DynamoDbPartitionKey)
     private String email;
+    @Getter(onMethod_=@DynamoDbSortKey)
     private int taskId;
     private String name;
-
-    @DynamoDbPartitionKey
-    public String getEmail() {
-        return email;
-    }
-
-    @DynamoDbSortKey
-    public int getTaskId() {
-        return taskId;
-    }
 }
