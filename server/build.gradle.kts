184af06e51fc9511fa16fa90451feef2e0c05789
Christopher Tamayo <chriswtamayo@gmail.com>
2024-09-09 15:05:10 -0700

    Add JUnit, Mockito, and Jacoco dependencies

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
 }
@@ -31,9 +32,30 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation(platform("org.junit:junit-bom:5.11.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+	testImplementation("org.mockito:mockito-core:3.+")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+tasks.jacocoTestCoverageVerification {
+	violationRules {
+		rule {
+			limit {
+				minimum = "0.5".toBigDecimal()
+			}
+		}
+	}
 }
