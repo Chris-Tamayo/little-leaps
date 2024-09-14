3acfaf3917c3f1d61525876b1feaed9882fa8f42
Christopher Tamayo <chriswtamayo@gmail.com>
2024-09-13 17:31:30 -0700

    Add motivations table

--- infrastructure/lib/ddb-table-stack.ts
+++ infrastructure/lib/ddb-table-stack.ts
@@ -5,17 +5,20 @@ import { Construct } from 'constructs';
 export class DDBTableStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const tableName = 'Tasks';
-    const partitionKeyName = 'email';
-    const sortKeyName = 'taskId';
     
-    const tableProps = {
-      tableName: tableName,
-      partitionKey: { name: partitionKeyName, type: AttributeType.STRING },
-      sortKey: { name: sortKeyName, type: AttributeType.NUMBER }
+    const taskTableProps = {
+      tableName: 'Tasks',
+      partitionKey: { name: 'email', type: AttributeType.STRING },
+      sortKey: { name: 'taskId', type: AttributeType.NUMBER }
+    }
+
+    const motivationTableProps = {
+      tableName: 'Motivations',
+      partitionKey: { name: 'email', type: AttributeType.STRING },
+      sortKey: { name: 'motivationId', type: AttributeType.NUMBER }
     }
     
-    new aws_dynamodb.TableV2(this, tableName, tableProps);
+    new aws_dynamodb.TableV2(this, 'Tasks', taskTableProps);
+    new aws_dynamodb.TableV2(this, 'Motivations', motivationTableProps);
   }
 }
--- infrastructure/test/ddb-table.test.ts
+++ infrastructure/test/ddb-table.test.ts
@@ -2,10 +2,16 @@ import { App } from 'aws-cdk-lib';
 import { DDBTableStack } from '../lib/ddb-table-stack';
 import { Template } from 'aws-cdk-lib/assertions';
 
-test('DDB Table Created', () => {
+test('DDB Tables Created', () => {
   const app = new App();
   const stack = new DDBTableStack(app, 'DDBTableTestStack');
   const template = Template.fromStack(stack);
 
-  template.hasResource('AWS::DynamoDB::GlobalTable', {});
+  template.hasResourceProperties('AWS::DynamoDB::GlobalTable', {
+    TableName: 'Tasks'
+  });
+
+  template.hasResourceProperties('AWS::DynamoDB::GlobalTable', {
+    TableName: 'Motivations'
+  });
 })
