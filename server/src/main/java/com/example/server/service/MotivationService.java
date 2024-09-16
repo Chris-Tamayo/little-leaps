fbc0322e143bed3cb26ca9480810dad80b5341c6
Christopher Tamayo <chriswtamayo@gmail.com>
2024-09-16 13:53:45 -0700

    Add motivation service and tests

---
+++ server/src/main/java/com/example/server/service/MotivationService.java
@@ -0,0 +1,58 @@
+package com.example.server.service;
+
+import com.example.server.dao.MotivationDAO;
+import com.example.server.model.Motivation;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class MotivationService {
+    private MotivationDAO motivationDAO;
+
+    /**
+     * Retrieve Motivation object based on primary key.
+     * @param email - Partition key
+     * @param motivationId - Sort key
+     * @return Retrieved Motivation from database
+     */
+    public Motivation findByPrimaryKey(final String email, final int motivationId) {
+        return motivationDAO.findByPrimaryKey(email, motivationId);
+    }
+
+    /**
+     * Save Motivation object in database.
+     * @param motivation - Motivation object to save
+     */
+    public void save(final Motivation motivation) {
+        motivationDAO.save(motivation);
+    }
+
+    /**
+     * Update Motivation object in database.
+     * @param motivation - Motivation object to update
+     * @return Updated Motivation from database
+     */
+    public Motivation update(final Motivation motivation) {
+        return motivationDAO.update(motivation);
+    }
+
+    /**
+     * Delete Motivation object based on primary key.
+     * @param email - Partition key
+     * @param motivationId - Sort key
+     * @return Deleted Motivation from database
+     */
+    public Motivation delete(final String email, final int motivationId) {
+        return motivationDAO.delete(email, motivationId);
+    }
+
+    /**
+     * Delete Motivation object in database by object.
+     * @param motivation - Motivation to delete
+     * @return Deleted Motivation object from database
+     */
+    public Motivation delete(final Motivation motivation) {
+        return motivationDAO.delete(motivation);
+    }
+}
---
+++ server/src/test/java/com/example/server/service/MotivationServiceTests.java
@@ -0,0 +1,67 @@
+package com.example.server.service;
+
+import com.example.server.dao.MotivationDAO;
+import com.example.server.model.Motivation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class MotivationServiceTests {
+    private final String EMAIL = "user@gmail.com";
+    private final int MOTIVATION_ID = 1;
+    private final String TEXT = "text";
+    private final Motivation MOTIVATION = new Motivation(EMAIL, MOTIVATION_ID, TEXT);
+
+    @Mock
+    private MotivationDAO motivationDAO;
+
+    @InjectMocks
+    private MotivationService motivationService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testFindByPrimaryKey() {
+        Mockito.when(motivationDAO.findByPrimaryKey(EMAIL, MOTIVATION_ID))
+                .thenReturn(MOTIVATION);
+        final Motivation retrievedMotivation = motivationService.findByPrimaryKey(EMAIL, MOTIVATION_ID);
+        Assertions.assertEquals(MOTIVATION, retrievedMotivation);
+    }
+
+    @Test
+    public void testSave() {
+        Mockito.doNothing().when(motivationDAO).save(MOTIVATION);
+        motivationService.save(MOTIVATION);
+        Mockito.verify(motivationDAO).save(MOTIVATION);
+    }
+
+    @Test
+    public void testUpdate() {
+        Mockito.when(motivationDAO.update(MOTIVATION))
+                .thenReturn(MOTIVATION);
+        final Motivation updatedMotivation = motivationService.update(MOTIVATION);
+        Assertions.assertEquals(MOTIVATION, updatedMotivation);
+    }
+
+    @Test
+    public void testDeleteByPrimaryKey() {
+        Mockito.when(motivationDAO.delete(EMAIL, MOTIVATION_ID))
+                .thenReturn(MOTIVATION);
+        final Motivation deletedMotivation = motivationService.delete(EMAIL, MOTIVATION_ID);
+        Assertions.assertEquals(MOTIVATION, deletedMotivation);
+    }
+
+    @Test
+    public void testDelete() {
+        Mockito.when(motivationDAO.delete(MOTIVATION)).thenReturn(MOTIVATION);
+        final Motivation deletedMotivation = motivationService.delete(MOTIVATION);
+        Assertions.assertEquals(deletedMotivation, MOTIVATION);
+    }
+}
