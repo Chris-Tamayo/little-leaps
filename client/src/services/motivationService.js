f6763d2a501d5cf61bd27047fd9102c205c65425
Christopher Tamayo <chriswtamayo@gmail.com>
2024-09-19 17:15:00 -0700

    Add API interface

--- client/src/services/motivationService.js
+++ client/src/services/motivationService.js
@@ -1,24 +1,30 @@
-const axios = require('axios').default;
+import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'http://localhost:8080',
   headers: {
-    'Content-Type': 'application.json',
+    'Content-Type': 'application/json',
   }
 });
 
 export const getMotivation = (motivation) => {
+  console.log("GET request: ", motivation);
   return instance.get('/motivation', motivation);
 }
 
 export const createMotivation = (motivation) => {
+  console.log("POST request: ", motivation);
   return instance.post('/motivation', motivation);
 }
 
 export const updateMotivation = (motivation) => {
+  console.log("PUT request: ", motivation);
   return instance.put('/motivation', motivation);
 }
 
 export const deleteMotivation = (motivation) => {
-  return instance.delete('/motivation', motivation);
+  console.log("DELETE request: ", motivation);
+  const email = motivation.email;
+  const id = motivation.id;
+  return instance.delete(`/motivation?email=${email}&motivationId=${id}`, motivation);
 }
